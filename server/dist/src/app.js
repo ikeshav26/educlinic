bc22b67f537ed4a500df902f9f3fa840117e0c99
ikeshav26 <keshavgilhotra4@gmail.com>
2026-04-14 13:37:45 +0530

    Init

---
+++ server/dist/src/app.js
@@ -0,0 +1,7 @@
+import express, {} from 'express';
+const app = express();
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+export default app;
+//# sourceMappingURL=app.js.map
